6bc35abf8e7e0c11e0c7ede8635106cd44b1c117
lastdman <domlai@hotmail.com>
2016-04-20 16:59:38 -0700

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,10 +1,14 @@
 var endPoint = "PROD";
 
+function setEndPoint (newEndPoint) {
+  endPoint = newEndPoint;
+  document.getElementById("endPoint").innerHTML = endPoint;
+}
+
 function toggleEndPoint() {
   if (endPoint == "PROD") {
     endPoint = "INTERN";
   } else {
     endPoint = "PROD";
   }
-  document.getElementById("endPoint").innerHTML = endPoint;
 }
