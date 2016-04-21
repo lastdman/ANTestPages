6b0a5ca05ced54fb6ae9759be436e6b83b7cb630
lastdman <domlai@hotmail.com>
2016-04-21 15:53:28 -0700

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,4 +1,5 @@
 var endPoint = "PROD";
+var endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
 
 // Self invoke function
 var invokeSetEndPoint = (function setEndPoint () {
@@ -9,8 +10,10 @@ var invokeSetEndPoint = (function setEndPoint () {
 function toggleEndPoint() {
   if (endPoint == "PROD") {
     endPoint = "INTERN";
+    endPointURL = "//www.tonypan.sb.facebook.com/assets.php/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
   } else {
     endPoint = "PROD";
+    endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
   }
   invokeSetEndPoint();
 }
