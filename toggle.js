ecd241a3614a3ee474f8ed0c78148792838781b8
lastdman <domlai@hotmail.com>
2016-04-25 11:41:20 -0700

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,20 +1,30 @@
-var endPoint = "PROD";
-var endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
+sessionStorage.setItem("endPoint", "PROD");
+sessionStorage.setItem("endPointURL", "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764");
+//var endPoint = "PROD";
+//var endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
 
 // Self invoke function
 var invokeSetEndPoint = (function setEndPoint () {
-  document.getElementById("endPoint").innerHTML = endPoint;
-  document.getElementById("endPointURL").innerHTML = endPointURL;
+  document.getElementById("endPoint").innerHTML = sessionStorage.getItem("endPoint");
+  document.getElementById("endPointURL").innerHTML = sessionStorage.getItem("endPointURL");
   return setEndPoint;
 })();
 
+function setEndPointToProd() {
+  sessionStorage.endPoint = "PROD";
+  sessionStorage.endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
+}
+
+function setEndPointToIntern() {
+  sessionStorage.endPoint = "INTERN";
+  sessionStorage.endPointURL = "//www.tonypan.sb.facebook.com/assets.php/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
+}
+
 function toggleEndPoint() {
-  if (endPoint == "PROD") {
-    endPoint = "INTERN";
-    endPointURL = "//www.tonypan.sb.facebook.com/assets.php/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
+  if (sessionStorage.endPoint == "PROD") {
+    setEndPointToIntern();
   } else {
-    endPoint = "PROD";
-    endPointURL = "//connect.facebook.net/en_US/sdk/xfbml.ad.js#xfbml=1&version=v2.5&appId=191347624543764";
+    setEndPointToProd();
   }
   invokeSetEndPoint();
 }
